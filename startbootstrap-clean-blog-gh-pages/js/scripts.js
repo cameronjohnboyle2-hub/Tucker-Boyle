40e3a53a0517d6c59d9542aaf08607655d77e00f
cameronjohnboyle2-hub <cameronjohnboyle2@gmail.com>
2026-04-22 23:10:19 -0600

    Add lightweight Clean Blog navigation script

---
+++ startbootstrap-clean-blog-gh-pages/js/scripts.js
@@ -0,0 +1,32 @@
+/*!
+* Simplified Clean Blog navigation behavior.
+*/
+window.addEventListener("DOMContentLoaded", () => {
+  let scrollPos = 0;
+  const mainNav = document.getElementById("mainNav");
+
+  if (!mainNav) {
+    return;
+  }
+
+  const headerHeight = mainNav.clientHeight;
+
+  window.addEventListener("scroll", () => {
+    const currentTop = document.body.getBoundingClientRect().top * -1;
+
+    if (currentTop < scrollPos) {
+      if (currentTop > 0 && mainNav.classList.contains("is-fixed")) {
+        mainNav.classList.add("is-visible");
+      } else {
+        mainNav.classList.remove("is-visible", "is-fixed");
+      }
+    } else {
+      mainNav.classList.remove("is-visible");
+      if (currentTop > headerHeight && !mainNav.classList.contains("is-fixed")) {
+        mainNav.classList.add("is-fixed");
+      }
+    }
+
+    scrollPos = currentTop;
+  });
+});
